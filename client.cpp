47b36911e34b32399e9934e631832ec17921dbf3
seerbann <serbypwp@yahoo.com>
2023-10-29 15:31:23 +0200

    reading s2c still not working

--- client.cpp
+++ client.cpp
@@ -12,13 +12,24 @@ using namespace std;
 
 int main()
 {
-    const char *FIFO = "MY_FIFO";
+    const char *FIFO1 = "MY_FIFO_c2s";
+    const char *FIFO2 = "MY_FIFO_s2c";
     char currCommand[100];
-    int c2s = open(FIFO, O_WRONLY); // client to server, write only
-
-    while (cin >> currCommand)
+    int c2s = open(FIFO1, O_WRONLY); // client to server, write only
+    int s2c = open(FIFO2, O_RDONLY);
+    int ok = 1;
+    char textToReceive[100];
+    while (cin >> currCommand && ok == 1)
     {
+        if (strcmp(currCommand, "quit") == 0)
+            ok = 0;
         write(c2s, currCommand, sizeof(currCommand));
+
+        if (read(s2c, textToReceive, 10) != -1)
+        {
+            printf("%s",textToReceive);
+            printf("\n");
+        }
     }
     /* Close the FIFO */
     close(c2s);
--- server.cpp
+++ server.cpp
@@ -13,10 +13,14 @@ vector<string> commands;
 int main()
 {
     /* Create the FIFO or die trying */
-    const char *FIFO1 = "MY_FIFO";
+    const char *FIFO1 = "MY_FIFO_c2s";
     mknod(FIFO1, S_IFIFO | 0666, 0);
     int c2s = open(FIFO1, O_RDONLY); // client to server , read only
-        
+
+    const char *FIFO2 = "MY_FIFO_s2c";
+    mknod(FIFO2, S_IFIFO | 0666, 0);
+    int s2c = open(FIFO2, O_WRONLY); // server to client , write only
+
     /* Try to open the FIFO. Delete FIFO if open() fails */
 
     if (c2s == -1)
@@ -26,23 +30,40 @@ int main()
         return 1;
     }
 
-    /* Main server loop */ 
+    /* Main server loop */
+
+    char currCommand[1024];
+    int bytes_read;
+    char textToSend[100];
+
     while (1)
     {
-        char currCommand[1024];
-        int bytes_read;
         bytes_read = read(c2s, currCommand, sizeof(currCommand));
-        if(bytes_read<=0)
-            break;    
-        
-        /* If we read quit, quit; otherwise print valid comand */
-        if (strcmp(currCommand,"quit")==0)
+        if (bytes_read <= 0)
             break;
-        printf("valid command\n");
+
+        /* If we read quit, quit; otherwise print valid comand */
+        if (strcasecmp(currCommand, "quit") == 0)
+            break;
+        else if (strcasecmp(currCommand, "login") == 0)
+        {
+            write(s2c, textToSend, sizeof(textToSend));
+        }
+
+        else if (strcasecmp(currCommand, "get-logged-users") == 0)
+            printf("get-logged-users\n");
+        else if (strcasecmp(currCommand, "get-proc-info") == 0)
+            printf("get-proc-info\n");
+        else if (strcasecmp(currCommand, "logout") == 0)
+            printf("logout\n");
+        else
+            printf("invalid command\n");
     }
 
-    /* Read QUIT from the FIFO, so close and delete the FIFO */
+    /* close and delete the FIFOs */
     close(c2s);
-    printf("Deleting FIFO\n");
+    close(s2c);
+    printf("Deleting FIFOs\n");
     unlink(FIFO1);
+    unlink(FIFO2);
 }
